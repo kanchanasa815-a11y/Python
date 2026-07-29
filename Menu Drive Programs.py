be78b620735e8a2364ff64936543cce434e12265
Kanchana S A <kanchanasa815@gmail.com>
2026-07-29 07:42:51 +0530

    Update Menu Drive Programs.py

--- Menu Drive Programs.py
+++ Menu Drive Programs.py
@@ -1,30 +1,30 @@
-print("Simple Caluclator")
+rint("Simple Caluclator")
 print("1.Addition")
 print("2.substraction")
 print("3.Multiplication")
 print("4.Division")
 print("5.Exit")
-choice=int(input("Enter the choice(1-5):"))
 while(True):
-if choice==1:
-    num1=int(input("Enter the number1: "))
-    num2=int(input("Enter the number2: "))
-    print(num1+num2)
-elif choice==2:
-    num1=int(input("Enter the number1: "))
-    num2=int(input("Enter the number2: "))
-    print(num1-num2)
-elif choice==3:
-    num1=int(input("Enter the number1: "))
-    num2=int(input("Enter the number2: "))
-    print(num1*num2)
-elif choice==4:
-    num1=int(input("Enter the number1: "))
-    num2=int(input("Enter the number2: "))
-    print(num1/num2)
-    Break
-else:
-    print("Exiting the calculatoor.GoodBye!")
+    choice=int(input("Enter the choice(1-5):"))
+    if choice==1:
+        num1=int(input("Enter the number1: "))
+        num2=int(input("Enter the number2: "))
+        print(num1+num2)
+    elif choice==2:
+        num1=int(input("Enter the number1: "))
+        num2=int(input("Enter the number2: "))
+        print(num1-num2)
+    elif choice==3:
+        num1=int(input("Enter the number1: "))
+        num2=int(input("Enter the number2: "))
+        print(num1*num2)
+    elif choice==4:
+        num1=int(input("Enter the number1: "))
+        num2=int(input("Enter the number2: "))
+        print(num1/num2)
+        break
+    else:
+        print("Exiting the calculatoor.GoodBye!")
  Output:
 Simple Caluclator
 1.Addition
