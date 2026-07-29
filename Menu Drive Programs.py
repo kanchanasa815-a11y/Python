eb90f0b23e6dddd22af26d006ecc0fbb49e4409d
Kanchana S A <kanchanasa815@gmail.com>
2026-07-29 16:33:03 +0530

    Update Menu Drive Programs.py

--- Menu Drive Programs.py
+++ Menu Drive Programs.py
@@ -34,3 +34,35 @@ Simple Caluclator
 5.Exit
 Enter the choice(1-5):5
 Exiting the calculatoor.GoodBye!
+
+def menu():
+    print("Welcome to the Menu-Driven Program!")
+    print("1. Option 1")
+    print("2. Option 2")
+    print("3. Option 3")
+    print("4. Exit")
+
+while True:
+    menu()
+    choice = input("Enter your choice (1-4): ")
+    
+    if choice == '1':
+        print("You selected Option 1.")
+    elif choice == '2':
+        print("You selected Option 2.")
+    elif choice == '3':
+        print("You selected Option 3.")
+    elif choice == '4':
+        print("Exiting the program. Goodbye!")
+        break
+    else:
+        print("Invalid choice. Please try again.")
+Output:
+Welcome to the Menu-Driven Program!
+1. Option 1
+2. Option 2
+3. Option 3
+4. Exit
+Enter your choice (1-4): 4
+Exiting the program. Goodbye!
+ 
