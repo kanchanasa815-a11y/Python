a6bde6a7f16b4bd1843454dc42fc6d94f5c81168
Kanchana S A <kanchanasa815@gmail.com>
2026-07-29 17:27:33 +0530

    Update Menu Drive Programs.py

--- Menu Drive Programs.py
+++ Menu Drive Programs.py
@@ -90,4 +90,57 @@ while(True):
 
         else:
               print("Exiting the calculatoor.GoodBye!")
+QUESTION:
+Banking System: Write a menu-driven program to simulate a basic banking system with options like:
+
+Check Balance
+Deposit Money
+Withdraw Money
+Exit
+
+print("Banking system")
+print("1.Check Balance")    
+print("2.Deposit Money")
+print("3.Withdraw Money")
+print("4.Exit")
+balance=0
+while True:
+    choice=int(input("Enter the choice:  "))
+    if choice in {1,2,3,4}:
+        if choice ==1:
+            print("Balance is checking",balance)
+        elif choice==2:
+            amount=int(input("Enter the amount to deposit: "))
+            balance+=amount
+            print("Deposit Money")
+        elif choice==3:
+            amount=int(input("Enter the amount to Withdraw: "))
+            if balance>amount:
+                balance-=amount
+            else:
+                print('Insufficient amount')
+            
+        elif choice==4:
+             print("Exit form the Banking system")
+             break
+        else:
+            print("There is no any Update.......")
+OUTPUT:
+Banking system
+1.Check Balance
+2.Deposit Money
+3.Withdraw Money
+4.Exit
+Enter the choice:  1
+Balance is checking 0
+Enter the choice:  2
+Enter the amount to deposit: 800
+Deposit Money
+Enter the choice:  3
+Enter the amount to Withdraw: 90
+Enter the choice:  4
+Exit form the Banking system
+
+
+
  
